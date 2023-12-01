4049b883ae3dff14d0be1815bb842efd0c611ae1
arocen <aufger1913@hotmail.com>
2023-12-01 14:08:28 +0800

    add find_docx_files function

--- doc2txt.py
+++ doc2txt.py
@@ -7,13 +7,26 @@ import os
 
 load_dotenv()
 
-directory = glob.glob(os.environ.get(""))
 
-for file_name in directory:
-    with open(file_name, 'rb') as infile:
-        with open(file_name[:-5]+'.txt', 'w', encoding='utf-8') as outfile:
-            doc = docx2txt.process(infile)
-            outfile.write(doc)
 
-print("=========")
-print("All done!")
+def find_docx_files(root_dir):
+    '''
+    Get all paths of docx files given a root directort.
+    '''
+    docx_files = []
+    for foldername, subfolders, filenames in os.walk(root_dir):
+        for filename in filenames:
+
+            # Types that are filtered out: jpg, jpeg, bmp, mov, png, pptx, wmv, mp4, pdf
+            if filename.endswith('.docx') or filename.endswith('.doc'):
+                docx_files.append(os.path.join(foldername, filename))
+    return docx_files
+
+# Replace 'your_root_directory' with the actual root directory
+root_directory = os.environ.get("cases_parent_directory")
+docx_paths = find_docx_files(root_directory)
+
+# Print the paths of all .docx files
+for docx_path in docx_paths:
+    print(docx_path)
+

d557e959c0e8b106242deb0d035083b5a9d00db5
arocen <aufger1913@hotmail.com>
2023-12-02 12:09:46 +0800

    completed doc2txt.py

--- doc2txt.py
+++ doc2txt.py
@@ -1,6 +1,6 @@
 # Convert docx files to txt.
 
-import docx2txt
+import pypandoc
 import glob
 from dotenv import load_dotenv
 import os
@@ -22,11 +22,26 @@ def find_docx_files(root_dir):
                 docx_files.append(os.path.join(foldername, filename))
     return docx_files
 
+
+def docx2txt(docx_paths:list):
+    '''Convert docx files to txt.'''
+    for path in docx_paths:
+        txt_path = path.split(".")[0] + ".txt"
+        try:
+            output = pypandoc.convert_file(path, 'plain', outputfile=txt_path)
+            assert output == ""
+        except:
+            print("Error convering doc file in this path:", path)
+    return
+
+
 # Replace 'your_root_directory' with the actual root directory
 root_directory = os.environ.get("cases_parent_directory")
 docx_paths = find_docx_files(root_directory)
 
 # Print the paths of all .docx files
-for docx_path in docx_paths:
-    print(docx_path)
+# for docx_path in docx_paths:
+#     print(docx_path)
 
+# Convert to txt
+docx2txt(docx_paths)
